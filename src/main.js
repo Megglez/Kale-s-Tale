11eeb39daa831cf2839a74785ee7e370d0f9f55b
Megglez <megan.laisa24@gmail.com>
2024-12-10 14:17:07 +0200

    added picture "slideshow" to index.html

--- src/main.js
+++ src/main.js
@@ -12,3 +12,18 @@ function backHome() {
   window.location.href = "index.html";
 }
 window.backHome = backHome;
+
+var counter = 1;
+function displayNextImage() {
+  var image = document.getElementById("welcomeKale");
+  image.src = "images/slide-" + counter + ".JPEG";
+  counter = counter + 1;
+
+  if (counter == 6) {
+    counter = 1;
+  }
+
+}
+setInterval(displayNextImage, 3000);
+
+displayNextImage();
